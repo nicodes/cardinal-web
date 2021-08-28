1f1c6a5be29847052d101fcad40c597b73c2990b
nicodes <nico@ni.codes>
2021-08-27 23:03:43 -0600

    add scss

--- src/src/App.tsx
+++ src/src/App.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
+import styles from './app.module.scss';
 
+// this doesn't work in scss
 const headerHeight = '60px';
 
 function App(): JSX.Element {
@@ -7,53 +9,18 @@ function App(): JSX.Element {
     <div className='App'>
       <header style={{ height: headerHeight }}>Cardinal Token</header>
       <main
-        style={{
-          height: `calc(100vh - ${headerHeight})`,
-          width: '100%',
-          overflowY: 'scroll',
-          scrollSnapType: `y mandatory`,
-        }}
+        className={styles.main}
+        style={{ height: `calc(100vh - ${headerHeight})` }}
       >
-        <div
-          style={{
-            height: '100%',
-            scrollSnapAlign: 'center',
-            backgroundImage: `url("https://images.unsplash.com/photo-1620656621654-ba94976cc312?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2700&q=80")`,
-            backgroundPosition: 'center',
-            backgroundSize: 'cover',
-          }}
-        >
-          <h1 style={{ color: 'white' }}>Cardinal Token</h1>
-          <h2 style={{ color: 'white' }}>Cardinals Soar</h2>
+        <div>
+          <h1>Cardinal Token</h1>
+          <h2>Cardinals Soar</h2>
         </div>
-
-        <div
-          style={{
-            height: '100%',
-            scrollSnapAlign: 'center',
-            backgroundColor: 'orange',
-          }}
-        >
-          SECTION 1
-        </div>
-        <div
-          style={{
-            height: '100%',
-            scrollSnapAlign: 'center',
-            backgroundColor: 'blue',
-          }}
-        >
-          SECTION 2
-        </div>
-        <div
-          style={{
-            height: '100%',
-            scrollSnapAlign: 'center',
-            backgroundColor: 'red',
-          }}
-        >
-          SECTION 3
-        </div>
+        <div>About - blurb with video</div>
+        <div>Smart Contract - bullet rules with diagram</div>
+        <div>How to buy</div>
+        <div>Roadmap</div>
+        <div>Social Links</div>
       </main>
     </div>
   );
