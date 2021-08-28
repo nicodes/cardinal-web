9d3b17d5d6375434e3bdf8c21ffb039a2c0345a1
nicodes <nico@ni.codes>
2021-08-28 15:12:41 -0600

    add temp logo to header

--- src/components/common/header/Header.tsx
+++ src/components/common/header/Header.tsx
@@ -1,18 +1,20 @@
 import React from 'react';
-import { Dropdown, DropdownButton } from 'react-bootstrap';
+import { Button, Dropdown, DropdownButton } from 'react-bootstrap';
 
 import { SocialLinks } from '..';
 import styles from './header.module.scss';
 
+import logo from '../../../assets/logo.png';
+
 function Header(): JSX.Element {
   return (
     <header className={styles.header}>
+      <img src={logo} />
       <div>Cardinal Token</div>
+      <Button variant='primary'>Dashboard</Button>
       <div className={styles.price}>price</div>
-      <div style={{ marginRight: 'auto', marginLeft: 'auto' }}>
-        <SocialLinks />
-      </div>
 
+      <SocialLinks />
       <DropdownButton id='dropdown-basic-button' title='Dropdown button'>
         <Dropdown.Item href='#/action-1'>Buy</Dropdown.Item>
         <Dropdown.Item href='#/action-2'>About</Dropdown.Item>
