a106a85b18b225c04c93dda866c8412364c331f2
nicodes <nico@ni.codes>
2021-08-28 19:29:21 -0600

    update splash

--- src/components/sections/splash/Splash.tsx
+++ src/components/sections/splash/Splash.tsx
@@ -8,6 +8,7 @@ function Social(): JSX.Element {
   return (
     <div className={styles.splash}>
       <h1>Cardinal Token</h1>
+      <h2>Address: 0x######5e3f0b99fcd83e9c089bddd6c7fce5c611</h2>
       <img src={logo} />
       <div className={styles.buttons}>
         <Button variant='primary'>Buy Now</Button>
